8be624e05d65d8fffa3b931ee122fafc8eb6db82
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-07-19 19:50:07 +0100

    Sending Messages

--- ws/chatroom.ts
+++ ws/chatroom.ts
@@ -1,18 +1,27 @@
 import {
   WebSocket,
+  isWebSocketCloseEvent,
 } from "https://deno.land/std/ws/mod.ts";
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 let sockets = new Map<string, WebSocket>();
 
 const chatConnection = async (ws: WebSocket) => {
-  console.log("new socket connection");
-
   // add new ws connection to map
   const uid = v4.generate();
   sockets.set(uid, ws);
 
-  console.log(sockets);
+  for await (const ev of ws) {
+    // delete socket if connection closed
+    if (isWebSocketCloseEvent(ev)) {
+      sockets.delete(uid);
+    }
+
+    // create ev object if ev is string
+    if (typeof ev === "string") {
+      let evObj = JSON.parse(ev);
+    }
+  }
 };
 
 export { chatConnection };
